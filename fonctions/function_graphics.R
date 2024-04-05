d507f09b796a96ecf5ad49073a990147a07da2b5
mael.pretet <mael.pretet@mnhn.fr>
2024-04-05 11:44:38 +0200

    feat: modification graphiques et travail sur les cartes animées

--- fonctions/function_graphics.R
+++ fonctions/function_graphics.R
@@ -1,7 +1,33 @@
 # Fonctions graphiques
 
+#########################################
+#------------- Par années --------------#
+#########################################
+
+# Histogramme + line
+histo_line <- function(df_histo, x_h = "date", y_h = "sum_ab",
+                       df_ligne, x_l = "date", y_l = "n",
+                       div, xtxt = "Date de collection", ytxt = "Abondance",
+                       ytxtsec = "Nombre d'observations"){
+  
+  return(ggplot() +
+    geom_bar(data = df_histo, aes(x = !!sym(x_h), y = !!sym(y_h)),
+             color = "#8A173A", fill="#ab0739", stat="identity") +
+    geom_line(data = df_ligne, aes(x = !!sym(x_l), y = !!sym(y_l)*div, group = 1),
+              color = "#ff795c") +
+    theme_cowplot() +
+    scale_x_date(date_labels = "%Y-%b", date_breaks = "9 months") +
+    theme(axis.text.x = element_text(angle = 0, size = 8),
+          axis.title.y.left =  element_text(size = 12, color = "#ab0739"),
+          axis.title.y.right = element_text(size = 12, color = "#ff795c")) +
+    xlab(xtxt) +
+    ylab(ytxt) +
+    scale_y_continuous(sec.axis = sec_axis(~./div, name = ytxtsec)))
+}
+
 # Graphique en echarts4r
 aes_echarts <- function(plot_e, xlab, ylab, title, line_color){
+  
   plot_e <- plot_e %>%
     # e_bar(sum_ab) %>%
     e_legend(top = "3%") %>%
@@ -22,8 +48,13 @@ aes_echarts <- function(plot_e, xlab, ylab, title, line_color){
   return(plot_e)
 }
 
+#########################################
+#--------------- Cartes ----------------#
+#########################################
+
 # Carte d'abondance
 carte_ab <- function(shape_map, fill_map, fill_title, fill_color, fill_cat){
+  
   carte <- ggplot(shape_map) + 
     geom_sf(aes(fill = fill_map), show.legend = "fill") +
     scale_fill_manual(values = fill_color, breaks = fill_cat, drop = FALSE)+
@@ -32,3 +63,58 @@ carte_ab <- function(shape_map, fill_map, fill_title, fill_color, fill_cat){
   
   return(carte)
 }
+
+# Carte animée avec plotly
+gg_carte = function(an, df_sp, france){
+  df_mois = data.frame(mois = c("01", "02", "03", "04", "05", "06",
+                                "07", "08", "09", "10", "11", "12"),
+                       jardin_id = 0,
+                       latitude = NA,
+                       longitude = NA,
+                       sum_ab = 0)
+  df_migration = df_sp %>%
+    filter(annee == an) %>%
+    mutate(mois = strftime(date_collection, "%m")) %>%
+    group_by(mois, jardin_id, latitude, longitude) %>%
+    summarise(sum_ab = sum(abondance)) %>%
+    filter(sum_ab != 0) %>%
+    dplyr::union(df_mois)
+  
+  if (an == as.numeric(strftime(Sys.Date(), "%Y")) ){
+    df_migration = df_migration %>%
+      filter(mois <= strftime(Sys.Date(), "%m"))
+  }
+  
+  gg =ggplot(france) +
+    geom_sf(fill = "#f0f0f0", color = "#a0a0a0") +
+    geom_point(data = df_migration, aes(x = longitude, y=latitude, frame = mois), color = "red") +
+    theme_minimal()
+  
+  return(gg %>%
+           ggplotly()  %>%
+           animation_opts(transition = 0, frame = 1000))
+}
+
+# Carte pour un mois d'une année (pour création gif)
+gg_carte_mois = function(month, df_sp, france){
+  df_mois = data.frame(mois = month,
+                       jardin_id = 0,
+                       latitude = NA,
+                       longitude = NA,
+                       sum_ab = 0)
+  df_migration = df_sp %>%
+    mutate(mois = strftime(date_collection, "%m")) %>%
+    filter(mois == month) %>%
+    group_by(mois, jardin_id, latitude, longitude) %>%
+    summarise(sum_ab = sum(abondance)) %>%
+    filter(sum_ab != 0) %>%
+    dplyr::union(df_mois)
+  
+  gg = ggplot(france) +
+    geom_sf(fill = "#f0f0f0", color = "#a0a0a0") +
+    geom_point(data = df_migration, aes(x = longitude, y=latitude), color = "red") +
+    theme_minimal()
+  
+  return(gg)
+}
+
